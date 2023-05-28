8ea0796f568c5727d3e7141a07c04fa50bfa002e
tanonkim <robbins4bos@gmail.com>
2023-05-28 11:38:24 +0900

    Feature: Add Initial Settings

---
+++ src/test/java/com/alpaco/dev/DevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpaco.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
