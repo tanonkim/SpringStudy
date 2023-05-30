54fbfdaab219dd70adbe354fdb931fdeaab9a688
tanonkim <robbins4bos@gmail.com>
2023-05-30 13:34:30 +0900

    Feature: Add Review Controller

--- src/main/java/com/alpaco/dev/controller/ReviewController.java
+++ src/main/java/com/alpaco/dev/controller/ReviewController.java
@@ -1,15 +1,15 @@
 package com.alpaco.dev.controller;
 
 import com.alpaco.dev.dto.review.ReviewListResponseDto;
+import com.alpaco.dev.dto.review.ReviewSaveRequestDto;
+import com.alpaco.dev.dto.review.ReviewSaveResponseDto;
 import com.alpaco.dev.service.ReviewService;
 import com.alpaco.dev.util.BaseResponse;
 import com.alpaco.dev.util.exception.ReviewException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -33,5 +33,23 @@ public class ReviewController {
         if (reviewList.size() == 0) throw new ReviewException(INVALID_REQUEST);
 
         return new BaseResponse<>(reviewList);
+    }
+
+    @GetMapping("/reviews/{reviewId}")
+    public BaseResponse<ReviewListResponseDto> get(@PathVariable Long reviewId) {
+
+        ReviewListResponseDto review = reviewService.findById(reviewId);
+
+        return new BaseResponse<>(review);
+    }
+
+    @PostMapping("/reviews")
+    public BaseResponse<ReviewSaveResponseDto> createReview(@RequestParam("reservationId") Long reservationId,
+                                                            @RequestParam(value = "reviewImages", required = false) List<MultipartFile> reviewImages,
+                                                            @RequestParam(value = "score", required = false, defaultValue = "5") int score,
+                                                            @RequestParam(value = "content", required = false) String content) {
+        ReviewSaveRequestDto requestDto = ReviewSaveRequestDto.of(reviewImages, score, content);
+        ReviewSaveResponseDto save = reviewService.save(Long.valueOf(1), reservationId, requestDto);
+        return new BaseResponse<>(save);
     }
 }
